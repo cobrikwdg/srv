74dc6c775d12c233916e806ec68b411e9ea39820
cobrikwdg <141549962+cobrikwdg@users.noreply.github.com>
2023-08-06 11:56:10 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cj7lkfc5kgrc73eclol0-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "6jQJ3QlTf3mrfgwO6FiwKarvIwzmHRHe",
+    DB: "cobrikdb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
